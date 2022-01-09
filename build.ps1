83115f3d90f43b2b76964d13e4c26705f3f3589f
Thomas Shallenberger <twshal@gmail.com>
2022-01-08 18:56:34 -0600

    Got working clean and build running against test dir

--- build.ps1
+++ build.ps1
@@ -1,16 +1,69 @@
 # Setup source and destination paths
 $AppName = 'Ammonite'
+
 $Src = './src/*'
 $Dst = './build'
+
 $WowInstallDir = 'C:\'
 $WowDst = $WowInstallDir + $AppName
+
 $TocFile = $AppName + '.toc'
-
+$Build = $Dst + '/'
+$TocPath = $Build + $TocFile
+$BuildPath = $Build + '*'
 # Exclude: gitignore, build
-# Include: app, config, Ammonite.toc, init.lua, README, CHANGELOG
+# Include: Ammonite.toc, init.lua, README, CHANGELOG, app/
 
-# Clean target folder
-Get-ChildItem -Path $Dst -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
 
-# Copy src into build (or dest)
-Copy-Item -Path $Src -Destination $Dst -Recurse -Force
+try {
+    # Clean target folder
+    # Get-ChildItem -Path $Dst -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
+
+    $exists = Test-Path $Dst
+    if ( $exists) {
+        $CleanDst = $Dst + '/*'
+        Remove-Item -Recurse -Force $CleanDst
+    }
+    # Copy README, CHANGELOG
+    $filter = [regex] ".*(README|CHANGELOG)"
+    $bin = Get-ChildItem -Path '.' | Where-Object { $_.Name -match $filter }
+    foreach ($item in $bin) {
+        Copy-Item -Path $item.FullName -Destination $Dst
+    }
+    # Copy src into build (or dest)
+
+    Copy-Item -Path $Src -Destination $Dst -Recurse -Force
+}
+catch {
+    Write-Host $PSItem.Exception.Message -ForegroundColor RED
+}
+finally {
+    $Error.Clear()
+}
+
+
+try {
+    # Clean target wow install folder
+    $exists = Test-Path $WowDst
+    if ( $exists) {
+        $CleanWowDst = $WowDst + '/*'
+        Remove-Item -Recurse -Force $CleanWowDst
+    }
+    else {
+        New-Item -ItemType "directory" -Path $WowDst
+    }
+
+    # Install the TOC file
+
+    Copy-Item -Path $TocPath  -Destination $WowDst -Recurse -Force
+    # Copy src into build (or dest)
+    Copy-Item -Path $BuildPath -Destination $WowDst -Recurse -Force
+}
+catch {
+    Write-Host $PSItem.Exception.Message -ForegroundColor RED
+}
+finally {
+    $Error.Clear()
+}
+
+
---
+++ build/README.md
@@ -0,0 +1,3 @@
+# Ammonite
+
+WoW TBC Ammo Manager
