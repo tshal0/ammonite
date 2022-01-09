015b5051c98bbf486e4ca11c81e16f9515f22386
Thomas Shallenberger <twshal@gmail.com>
2022-01-09 04:22:08 -0600

    Added event handler that successfully catches bag inventory updates and updates ammo count

--- src/Ammonite.lua
+++ src/Ammonite.lua
@@ -18,6 +18,10 @@ local ALDB = LDB:NewDataObject("Ammonite", ldbOptions)
 local LDBIcon = LibStub("LibDBIcon-1.0")
 local AceCfg = LibStub("AceConfig-3.0")
 local AceCfgDlg = LibStub("AceConfigDialog-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
+local AceEvent = LibStub("AceEvent-3.0")
+
+AceEvent:Embed(Ammonite)
 
 local defaults = {
   profile = {
@@ -30,7 +34,7 @@ local defaults = {
   }
 }
 local AceDB = LibStub("AceDB-3.0")
-function Ammonite:Init()
+function Ammonite:InitOptions()
   Ammonite.enabled = true
   Ammonite.options = {
     type = "group",
@@ -95,7 +99,8 @@ end
 function Ammonite:OnInitialize()
   self.db = AceDB:New("AmmoniteDB", defaults)
   LDBIcon:Register("Ammonite", ALDB, self.db.profile.minimap)
-  Ammonite.Init()
+  Ammonite.InitOptions()
+  Ammonite.RegisterAmmoEvents()
   Ammonite:Print("Version: {{VERSION}}")
 end
 
@@ -103,14 +108,39 @@ function Ammonite:ShowOptions() AceCfgDlg:Open("Ammonite") end
 
 -- AmmoCounter
 
+function Ammonite:RegisterAmmoEvents()
+  Ammonite.events = {}
+  Ammonite:RegisterEvent("BAG_UPDATE", "UpdateAmmoCount")
+end
+
 function Ammonite:UpdateAmmoCount()
   local ammoSlot = GetInventorySlotInfo("AmmoSlot");
   local ammoCount = GetInventoryItemCount("player", ammoSlot);
   if ((ammoCount == 1) and (not GetInventoryItemTexture("player", ammoSlot))) then
     ammoCount = 0;
   end
-  Ammonite.db.profile.player.ammoCount = ammoCount
+  if (not Ammonite.ammoCount) then Ammonite.ammoCount = {value = 0} end
+  Ammonite.ammoCount.value = ammoCount
   local color = "00ff00"
   local message = "Ammo: " .. "|cff" .. color .. ("%.1f"):format(ammoCount)
   Ammonite:Print(message)
+
+  local LSM = LibStub("LibSharedMedia-3.0");
+  local font = LSM:Fetch("font", "MeatEdition");
+  local _, class = UnitClass("player");
+  local color = RAID_CLASS_COLORS[class];
+  if (not Ammonite.ammoCount.frame) then
+    Ammonite.ammoCount = {}
+    Ammonite.ammoCount.frame = CreateFrame("Frame", nil, UIParent)
+    Ammonite.ammoCount.frame:SetWidth(1)
+    Ammonite.ammoCount.frame:SetHeight(5)
+    Ammonite.ammoCount.frame:SetPoint("CENTER", 0, 0)
+    Ammonite.ammoCount.textFrame = Ammonite.ammoCount.frame:CreateFontString(
+                                     Ammonite.ammoCount.frame, "OVERLAY",
+                                     "GameTooltipText")
+
+    Ammonite.ammoCount.textFrame:SetPoint("CENTER", 0, 0);
+    Ammonite.ammoCount.textFrame:SetTextColor(color.r, color.g, color.b);
+  end
+  Ammonite.ammoCount.textFrame:SetText(message);
 end
