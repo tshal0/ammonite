ebb3e62f4bd55e74dd1651979cee769acc879c40
Thomas Shallenberger <twshal@gmail.com>
2022-01-09 00:01:56 -0600

    Added formatter

--- src/Ammonite.lua
+++ src/Ammonite.lua
@@ -1,29 +1,36 @@
 local _, Ammonite = ...
-Ammonite = LibStub("AceAddon-3.0"):NewAddon(Ammonite, 
-    "Ammonite", 
-    "AceConsole-3.0", 
-    "AceEvent-3.0", 
+Ammonite =
+  LibStub("AceAddon-3.0"):NewAddon(
+    Ammonite, "Ammonite",
+    "AceConsole-3.0", "AceEvent-3.0",
     "AceHook-3.0")
 _G.Ammonite = Ammonite
 
-local LDB = LibStub("LibDataBroker-1.1", true)
-local LDBIcon = LibStub("LibDBIcon-1.0", true)
+local LDB = LibStub("LibDataBroker-1.1",
+                    true)
+local LDBIcon = LibStub("LibDBIcon-1.0",
+                        true)
 
 local defaults = {
-    profile = {
-        modules = {
-            ['*'] = {enabled = true, visible = true},
-            moduleB = {enabled = false, visible = true}
-        }
+  profile = {
+    modules = {
+      ["*"] = {
+        enabled = true,
+        visible = true
+      },
+      ammoCounter = {
+        enabled = true,
+        visible = true
+      }
     }
+  }
 }
 function Ammonite:OnInitialize()
-    self.db = LibStub("AceDB-3.0"):New("AmmoniteDB", defaults)
-    Ammonite:Print("Version: {{VERSION}}")
+  self.db = LibStub("AceDB-3.0"):New(
+              "AmmoniteDB", defaults)
+  Ammonite:Print("Version: {{VERSION}}")
 end
 
-function Ammonite:OnEnable()
-end
+function Ammonite:OnEnable() end
 
-function Ammonite:OnDisable() 
-end
+function Ammonite:OnDisable() end
