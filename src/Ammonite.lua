3d33e9dbe9ce245ddcbbc01a042bdb0f3a5dd913
Thomas Shallenberger <twshal@gmail.com>
2022-01-09 02:16:29 -0600

    Added standalone config frame with ammo counter and minimap options

--- src/Ammonite.lua
+++ src/Ammonite.lua
@@ -1,77 +1,77 @@
 local _, Ammonite = ...
-Ammonite = LibStub("AceAddon-3.0"):NewAddon(Ammonite,
-                                            "Ammonite",
-                                            "AceConsole-3.0",
-                                            "AceEvent-3.0",
+Ammonite = LibStub("AceAddon-3.0"):NewAddon(Ammonite, "Ammonite",
+                                            "AceConsole-3.0", "AceEvent-3.0",
                                             "AceHook-3.0")
 _G.Ammonite = Ammonite
 
-local LDB =
-  LibStub("LibDataBroker-1.1", true):NewDataObject(
-    "Ammonite!", {
-      type = "data source",
-      text = "Ammonite!",
-      icon = "Interface\\Icons\\inv_misc_quiver_03",
-      OnClick = function()
-        Ammonite:Print("AmmoniteOptionsFrame:Show")
-        Ammonite:Print("Version: {{VERSION}}")
-        InterfaceOptionsFrame_OpenToCategory("Ammonite")
-        InterfaceOptionsFrame_OpenToCategory("Ammonite")
-      end
-    })
-local LDBIcon = LibStub("LibDBIcon-1.0")
-local AceConfig = LibStub("AceConfig-3.0")
-
-Ammonite.options = {
-  type = "group",
-  name = "Ammonite",
+local LDB = LibStub("LibDataBroker-1.1", true):NewDataObject("Ammonite", {
+  type = "data source",
+  text = "Ammonite",
   icon = "Interface\\Icons\\inv_misc_quiver_03",
-  childGroups = "tree",
-  plugins = {},
-  args = {
-    enable = {
-      name = "Enable",
-      desc = "Enables / disables the addon",
-      type = "toggle",
-      set = function(info, val) Ammonite.enabled = val end,
-      get = function(info) return Ammonite.enabled end
-    },
-    minimap = {
-      name = "Minimap",
-      type = "group",
-      args = {
-        show = {
-          name = "Show Minimap Button",
-          type = "toggle",
-          set = function(info, val)
-            Ammonite.db.profile.minimap.visible = val
-          end,
-          get = function(info)
-            return Ammonite.db.profile.minimap.visible
-          end
+  OnClick = function()
+    Ammonite:Print("Version: {{VERSION}}")
+    Ammonite:ShowOptions()
+  end
+})
+local LDBIcon = LibStub("LibDBIcon-1.0")
+
+function Ammonite:InitOptions()
+  Ammonite.enabled = true
+  Ammonite.options = {
+    type = "group",
+    name = "Ammonite",
+    icon = "Interface\\Icons\\inv_misc_quiver_03",
+    childGroups = "tree",
+    plugins = {},
+    args = {
+      enable = {
+        name = "Enable",
+        desc = "Enables / disables the addon",
+        type = "toggle",
+        set = function(info, val) Ammonite.enabled = val end,
+        get = function(info) return Ammonite.enabled end
+      },
+      minimap = {
+        name = "Minimap",
+        type = "group",
+        args = {
+          show = {
+            name = "Show Minimap Button",
+            type = "toggle",
+            set = function(info, val)
+              Ammonite.db.profile.minimap.visible = val
+            end,
+            get = function(info)
+              return Ammonite.db.profile.minimap.visible
+            end
+          }
         }
-      }
-    },
-    ac = {
-      name = "AmmoCounter",
-      type = "group",
-      args = {
-        enable = {
-          name = "Enable AmmoCounter",
-          type = "toggle",
-          set = function(info, val)
-            Ammonite.db.profile.modules.ammoCounter.enabled =
-              val
-          end,
-          get = function(info)
-            return Ammonite.db.profile.modules.ammoCounter
-                     .enabled
-          end
+      },
+      ac = {
+        name = "AmmoCounter",
+        type = "group",
+        args = {
+          enable = {
+            name = "Enable AmmoCounter",
+            type = "toggle",
+            set = function(info, val)
+              Ammonite.db.profile.modules.ammoCounter.enabled = val
+            end,
+            get = function(info)
+              return Ammonite.db.profile.modules.ammoCounter.enabled
+            end
+          }
         }
       }
     }
   }
-}
+  LibStub("AceConfig-3.0"):RegisterOptionsTable("Ammonite", Ammonite.options,
+                                                {"ammonite", "am"})
+  LibStub("AceConfigDialog-3.0"):SetDefaultSize("Ammonite", 680, 560)
+
+  self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(
+                        "Ammonite", "Ammonite", Ammonite.options);
+end
 
 local defaults = {
   profile = {
@@ -88,18 +88,12 @@ local AceGUI = LibStub("AceGUI-3.0")
 function Ammonite:OnInitialize()
   self.db = LibStub("AceDB-3.0"):New("AmmoniteDB", defaults)
   LDBIcon:Register("Ammonite", LDB, self.db.profile.minimap)
+  self:InitOptions()
 
-  AceConfig:RegisterOptionsTable("Ammonite",
-                                 Ammonite.options,
-                                 {"ammonite", "am"})
-  self.optionsFrame =
-    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(
-      "Ammonite", "Ammonite", "Options");
   Ammonite:Print("Version: {{VERSION}}")
 end
 function Ammonite:ToggleMinimapButton()
-  self.db.profile.minimap.visible =
-    not self.db.profile.minimap.visible
+  self.db.profile.minimap.visible = not self.db.profile.minimap.visible
 end
 
 -- function that draws the widgets for the first tab
@@ -139,13 +133,11 @@ local function SelectGroup(container, event, group)
   end
 end
 
-function Ammonite:ShowOptions()
+function Ammonite:DrawGroup()
   local frame = AceGUI:Create("Frame")
   frame:SetTitle("Ammonite")
   -- frame:SetStatusText("Ammonite Options")
-  frame:SetCallback("OnClose", function(widget)
-    AceGUI:Release(widget)
-  end)
+  frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
   frame:SetLayout("Fill")
 
   -- Create List Group with Modules
@@ -168,4 +160,9 @@ function Ammonite:ShowOptions()
 
   -- add to the frame container
   frame:AddChild(tab)
+end
+function Ammonite:ShowOptions()
+  -- InterfaceOptionsFrame_OpenToCategory(Ammonite.optionsFrame)
+  -- InterfaceOptionsFrame_OpenToCategory(Ammonite.optionsFrame)
+  LibStub("AceConfigDialog-3.0"):Open("Ammonite")
 end
