2f16ec7a97d428beda25c68bec944c98f7a48944
Thomas Shallenberger <twshal@gmail.com>
2022-01-09 03:16:36 -0600

    Added button that gets current ammo count

--- src/Ammonite.lua
+++ src/Ammonite.lua
@@ -3,8 +3,7 @@ Ammonite = LibStub("AceAddon-3.0"):NewAddon(Ammonite, "Ammonite",
                                             "AceConsole-3.0", "AceEvent-3.0",
                                             "AceHook-3.0")
 _G.Ammonite = Ammonite
-
-local LDB = LibStub("LibDataBroker-1.1", true):NewDataObject("Ammonite", {
+local ldbOptions = {
   type = "data source",
   text = "Ammonite",
   icon = "Interface\\Icons\\inv_misc_quiver_03",
@@ -12,10 +11,26 @@ local LDB = LibStub("LibDataBroker-1.1", true):NewDataObject("Ammonite", {
     Ammonite:Print("Version: {{VERSION}}")
     Ammonite:ShowOptions()
   end
-})
-local LDBIcon = LibStub("LibDBIcon-1.0")
+}
+local LDB = LibStub("LibDataBroker-1.1", true)
+local ALDB = LDB:NewDataObject("Ammonite", ldbOptions)
 
-function Ammonite:InitOptions()
+local LDBIcon = LibStub("LibDBIcon-1.0")
+local AceCfg = LibStub("AceConfig-3.0")
+local AceCfgDlg = LibStub("AceConfigDialog-3.0")
+
+local defaults = {
+  profile = {
+    minimap = {visible = true},
+    player = {ammoCount = 0},
+    modules = {
+      ["*"] = {enabled = true, visible = true},
+      ammoCounter = {enabled = true, visible = true}
+    }
+  }
+}
+local AceDB = LibStub("AceDB-3.0")
+function Ammonite:Init()
   Ammonite.enabled = true
   Ammonite.options = {
     type = "group",
@@ -60,109 +75,42 @@ function Ammonite:InitOptions()
             get = function(info)
               return Ammonite.db.profile.modules.ammoCounter.enabled
             end
+          },
+          getAmmoCount = {
+            name = "Get Ammo Count",
+            type = "execute",
+            func = function(info, val) Ammonite.UpdateAmmoCount() end
+
           }
         }
       }
     }
   }
-  LibStub("AceConfig-3.0"):RegisterOptionsTable("Ammonite", Ammonite.options,
-                                                {"ammonite", "am"})
-  LibStub("AceConfigDialog-3.0"):SetDefaultSize("Ammonite", 680, 560)
 
-  self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(
-                        "Ammonite", "Ammonite", Ammonite.options);
+  AceCfg:RegisterOptionsTable("Ammonite", Ammonite.options, {"ammonite", "am"})
+  AceCfgDlg:SetDefaultSize("Ammonite", 680, 560)
+  Ammonite.optionsFrame = AceCfgDlg:AddToBlizOptions("Ammonite", "Ammonite",
+                                                     Ammonite.options);
 end
-
-local defaults = {
-  profile = {
-    minimap = {visible = true},
-    modules = {
-      ["*"] = {enabled = true, visible = true},
-      ammoCounter = {enabled = true, visible = true}
-    }
-  }
-}
-
-local AceGUI = LibStub("AceGUI-3.0")
-
 function Ammonite:OnInitialize()
-  self.db = LibStub("AceDB-3.0"):New("AmmoniteDB", defaults)
-  LDBIcon:Register("Ammonite", LDB, self.db.profile.minimap)
-  self:InitOptions()
-
+  self.db = AceDB:New("AmmoniteDB", defaults)
+  LDBIcon:Register("Ammonite", ALDB, self.db.profile.minimap)
+  Ammonite.Init()
   Ammonite:Print("Version: {{VERSION}}")
 end
-function Ammonite:ToggleMinimapButton()
-  self.db.profile.minimap.visible = not self.db.profile.minimap.visible
-end
 
--- function that draws the widgets for the first tab
-local function DrawGroup1(container)
-  local desc = AceGUI:Create("Label")
-  desc:SetText("This is Tab 1")
-  desc:SetFullWidth(true)
-  container:AddChild(desc)
+function Ammonite:ShowOptions() AceCfgDlg:Open("Ammonite") end
 
-  local button = AceGUI:Create("Button")
-  button:SetText("Tab 1 Button")
-  button:SetWidth(200)
-  container:AddChild(button)
-end
+-- AmmoCounter
 
--- function that draws the widgets for the second tab
-local function DrawGroup2(container)
-  local desc = AceGUI:Create("Label")
-  desc:SetText("This is Tab 2")
-  desc:SetFullWidth(true)
-  container:AddChild(desc)
-
-  local button = AceGUI:Create("Button")
-  button:SetText("Tab 2 Button")
-  button:SetWidth(200)
-  container:AddChild(button)
-end
-
--- Callback function for OnGroupSelected
-
-local function SelectGroup(container, event, group)
-  container:ReleaseChildren()
-  if group == "tab1" then
-    DrawGroup1(container)
-  elseif group == "tab2" then
-    DrawGroup2(container)
+function Ammonite:UpdateAmmoCount()
+  local ammoSlot = GetInventorySlotInfo("AmmoSlot");
+  local ammoCount = GetInventoryItemCount("player", ammoSlot);
+  if ((ammoCount == 1) and (not GetInventoryItemTexture("player", ammoSlot))) then
+    ammoCount = 0;
   end
-end
-
-function Ammonite:DrawGroup()
-  local frame = AceGUI:Create("Frame")
-  frame:SetTitle("Ammonite")
-  -- frame:SetStatusText("Ammonite Options")
-  frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
-  frame:SetLayout("Fill")
-
-  -- Create List Group with Modules
-  -- Create Content Group
-  -- Add Module AmmoCounter
-  -- Add AmmoCounterOptions Tab
-
-  -- Create the TabGroup
-  local tab = AceGUI:Create("TabGroup")
-  tab:SetLayout("Flow")
-  -- Setup which tabs to show
-  tab:SetTabs({
-    {text = "Tab 1", value = "tab1"},
-    {text = "Tab 2", value = "tab2"}
-  })
-  -- Register callback
-  tab:SetCallback("OnGroupSelected", SelectGroup)
-  -- Set initial Tab (this will fire the OnGroupSelected callback)
-  tab:SelectTab("tab1")
-
-  -- add to the frame container
-  frame:AddChild(tab)
-end
-function Ammonite:ShowOptions()
-  -- InterfaceOptionsFrame_OpenToCategory(Ammonite.optionsFrame)
-  -- InterfaceOptionsFrame_OpenToCategory(Ammonite.optionsFrame)
-  LibStub("AceConfigDialog-3.0"):Open("Ammonite")
+  Ammonite.db.profile.player.ammoCount = ammoCount
+  local color = "00ff00"
+  local message = "Ammo: " .. "|cff" .. color .. ("%.1f"):format(ammoCount)
+  Ammonite:Print(message)
 end
